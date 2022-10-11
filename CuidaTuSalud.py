225a597a95d047793011232724c7e1f22a6a3670
elunacado <54047744+elunacado@users.noreply.github.com>
2022-10-10 21:09:09 -0500

    Se agrega el manejo de archivos

--- CuidaTuSalud.py
+++ CuidaTuSalud.py
@@ -3,8 +3,22 @@
 import math
 import datetime as dt
 
+#===========FUNCIONES PARA DEFINIR LOS VALORES===========#
+
 #funcion para saber si el sujeto es de sexo femenino o masculino
 def Genero(kC):
+    """
+    Uso de condicionales
+    Uso de  funciones
+    Uso de operadores
+    Recibe las kiloCalorias del alimento
+    A kiloCalorias lo multiplica por 1000 para 
+    despues multiplicarlo por .80 para despues 
+    restarle ya sea 8300 si el usuario es hombre
+    o 7100 si el usuario es una mujer 
+    Devuelve la cantidad de energia almacenada de acuerdo
+    al sexo del usuario
+    """
     genero=input("Es usted hombre o mujer H/M ")
 
     if genero == 'H':
@@ -19,6 +33,14 @@ def Genero(kC):
 
 #funcion para el nombre del usuario
 def Name():
+    """
+    Uso de ciclos While
+    Uso de funciones
+    Uso de operadores
+    le pide el nombre al usuario y en caso de que el
+    usuario no lo de el programa lo volvera a pedir
+    Devuelve el nombre
+    """
     name=input("Ingresa tu nombre ")
     while name=="":
         #Este le preguntara al usuario su nombre y en caso de no ingresar ninguno repetira la pregunta
@@ -27,6 +49,14 @@ def Name():
 
 #funcion para saber el peso del usuario
 def Weight():
+    """
+    Uso de ciclos While
+    uso de funciones
+    Uso de operadores
+    le pide el peso al usuario y en caso de que el
+    usuario no lo de el programa lo volvera a pedir
+    Devuelve el peso
+    """
     w=""
     w=int(input("Ingresa tu peso en kilogramos "))
     while w=="":
@@ -35,6 +65,14 @@ def Weight():
 
 #funcion para saber la altura del usuario
 def Height():
+    """
+    Uso de ciclos While
+    uso de funciones
+    Uso de operadores
+    le pide la altura al usuario y en caso de que el
+    usuario no lo de el programa lo volvera a pedir
+    Devuelve la altura
+    """
     h=""
     h=float(input("Ingresa tu altura en metros "))
     while h=="":
@@ -43,6 +81,15 @@ def Height():
 
 #funcion para saber que alimento quiere comer el usuario
 def Producto():
+    """
+    Uso de ciclos While
+    uso de funciones
+    Uso de operadores
+    le pide el nombre del producto al usuario y en caso de que el
+    usuario no lo de el programa lo volvera a pedir
+    Devuelve el nombre del producto
+
+    """
     producto=""
     producto=input("Que alimento quieres comer ")
     while producto=="":
@@ -51,11 +98,28 @@ def Producto():
 
 #funcion para imprimir la matriz donde se encuentra la rutina
 def imprimirRutina(rutina):
+    """
+    Uso de ciclos For
+    uso de funciones
+    Uso de matrices
+    Por cada n en el rango de 0 y la longitud de mi rutina
+    Imprime la n de mi rutina
+    """
     for n in range(0, len(rutina)):
         print(rutina[n])
 
 #funcion para saber si el usuario hace ejercicio y decide que clase de rutina quiere el usuario
 def hacesEjercicio(intensidad):
+    """
+    Usa operadores de decision
+    Usa matrices
+    Usa funciones
+    Si el usuario eligio la opcion de 1 o
+    el usuario elige la opcion 2 
+    Llama a la funcion de Imprimir Rutina
+    (pero su itensidad variara de acuerdo a la
+    intensidad elegida) 
+    """
     rutina=[[" L  "," M  "," M  "," J  "," V  "],[" EJ "," DE "," EJ "," DE "," EJ "],["EJ = Ejercicio","DE = Descanso"]]
     if intensidad == "1":
         print("Una buena rutina seria:")
@@ -72,16 +136,44 @@ def hacesEjercicio(intensidad):
 
 #funcion que contiene el menu
 def Menu(name):
+    """
+    Usa Funciones
+    Recibe el nombre
+    Imprime el menu utlizando el nombre del usuario
+    """
     print("Hola" ,name, "Bienvenido a la apliccion que cuida tu salud")
     print("1. Calcular tu IMC")
     print("2. Calcular la cantidad de calorias que absorbe tu cuerpo al comer cierto alimento")
     print("3. Sugiereme una rutina para realizar ejercicio")
     print("4. Dime el historial de mi IMC")
-    print("5. Salir")
-    print("7. Prueba de Funcionamiento")
+    print("5. Prueba de Funcionamiento")
+    print("6. Historial de TODOS los usuarios")
+    print("7. Salir")
 
 #funcion para calcular el IMC del usuario
 def calcularIMC(h, w):
+    """
+    Usa condicionales
+    Usa Funciones
+    Usa operadores booleanos
+
+    Recibe los valores de  h y w que son
+    h=altura del usuario
+    w=peso del usuario
+    
+    El IMC es igual a w/(h*h)
+    Si IMC es mayor o igual a 30
+    Tu condicion es de obesidad
+    Sino Si tu IMC es mayor o igual a 25
+    y menor o igual a 29.9
+    Tu condicion es de Sobrepeso
+    Sino Si tu IMC es mayor o igual a 18.5
+    y menor o igual a 24.9
+    Tu condicion es de Normal
+    Sin embargo si tu IMC es minor a 18.5
+    Tu condicion es de Desnutrido
+    Devuelve el IMC y la condicion
+    """
     IMC= w/(h*h)
     condicion=""
     if IMC >= 30:
@@ -97,57 +189,78 @@ def calcularIMC(h, w):
 
 #funcion para calcular la cantidad de energia que gana el usuario al ingerir cierto alimento
 def EnergiaGananda(name, w, producto, kC, resultadoXGenero,):
+    """
+    Usa Funciones
+    
+    Recibe los valores de nombre, w, producto, kC, resultadoXGenero
+    donde:
+        nombre: Nombre del usuario
+        w: peso del usuario
+        producto: nombre del producto del usuario
+        kC: Cantidad de kilocalorias del alimento del usuario
+        resultadoXGenero: Cantidad de Calorias Almacenadas por el usuario de acuerdo a su sexo
+    
+    saca la cantidad de calorias que se queman por el usuario a la hora de subir un escalon
+    y lo guarda en la variable de caloriasPorEscalon
+
+    por ultimo el resultado es igual a dividir resultadoXGenero/caloriasPorEscalon
+    
+    Imprime
+    name ," tu ",producto," hara que almacenes ", resultadoXGenero , " Kilocalorias por lo que tendras que subir ", math.ceil(result)," escalones para quemar tu alimento "
+
+    """
     caloriasPorEscalon=w*9.81*.15
     result=resultadoXGenero/caloriasPorEscalon
     print(name ," tu ",producto," hara que almacenes ", resultadoXGenero , " Kilocalorias por lo que tendras que subir ", math.ceil(result)," escalones para quemar tu alimento " )
 
-def main():
-    name=Name()
-    
-    historialImc=[]
+# Lee el archivo y lo imprime
+def leerFile(archivo):
+    """
+    usa archivo
+    usa funciones
+    recibe archivo
+
+    busca el primer elemento de el archivo
+    lee el archivo
+
+    imprime el contenido
+    """
+    print("El historial esta organizado en IMC/año")
+    file=archivo
+    file.seek(0)
+    contenido=file.read()
+    print(contenido)
+    file.close
+
+# Agrega al archivo el IMC y el año
+
+def appendFile(archivo, IMC):
+    """
+    usa archivo
+    usa funciones
+
+    recibe archivo y el IMC
+
+    agrega el IMC
+    agrega la fecha
+
+    """
     date=dt.date.today()
-    while True:
-        Menu(name)
-        menu=input("Selecciona el indice de la actividad que quieras realizar. 1-5 ")
-        if menu == "1":
-            h=Height()
-            w=Weight()
-            IMC, condicion=calcularIMC(h, w)
-            print("Tu IMC es de", round(IMC,2) ,"lo que significa que tu condicion es de ",condicion )
-            historialImc.append(name)
-            historialImc.append(IMC)
-            historialImc.append(date.year)
-        
-        elif menu=="2":
-            w=Weight()
-            producto=Producto()
-            kC=int(input("Cuantas kiloCalorias tiene tu alimento "))
-            resultadoXGenero=Genero(kC)
-            EnergiaGananda(name, w, producto,kC, resultadoXGenero)
-        
-        elif menu=="3":
-            intensidad=input("¿Prefieres una rutina de ejercicio intensa o relajada? 1=Intensa, 2=Relajada")
-            hacesEjercicio(intensidad)
+    print("El historial se muestra como IMC, Fecha")
 
-        elif menu=="4":
-            if len(historialImc) == 0:
-                print("No tienes historial de IMC")
-                print("")
-            else:
-                print(name, "tu IMC de manera cronologica ha sido de (IMC, año)")
-                print(historialImc)
-                print("")
+    archivo.write(int(round(IMC,2)))
+    archivo.write("\n")
+    archivo.write(str(date.year))
+    archivo.write("\n")
+    archivo.close()
 
-        else:
-            break
-
-main()
 
 def prueba():
+    
     name="DUMMY"
     historialImc=["John Doe", "27.8","2010"]
     date=dt.date.today()
-
+    
     #Menu
     Menu(name)
     print(" ")
@@ -203,7 +316,72 @@ def prueba():
     #['John Doe', '27.8', '2010', 'DUMMY', 27.78, 2022]
     print(historialImc)
     print("")
+    
+    archivoAp=open("historial.txt", "a")
+    date=dt.date.today()
+    archivoAp.write(str(round(IMC,2)))
+    archivoAp.write("\n")
+    archivoAp.write(str(date.year))
+    archivoAp.write("\n")
+    archivoAp.close()
+    archivoAp.close()
+    
+    archivoLeer=open("historial.txt", "r")
+    leerFile(archivoLeer)
+    archivoLeer.close()
+  
+def main():
+    name=Name()
+    
+    historialImc=[]
+    date=dt.date.today()
+    while True:
+        Menu(name)
+        menu=input("Selecciona el indice de la actividad que quieras realizar. 1-5 ")
+        if menu == "1":
+            h=Height()
+            w=Weight()
+            IMC, condicion=calcularIMC(h, w)
+            print("Tu IMC es de", round(IMC,2) ,"lo que significa que tu condicion es de ",condicion )
+            historialImc.append(name)
+            historialImc.append(IMC)
+            historialImc.append(date.year)
+            archivo=open("historial.txt", "a")
+            appendFile(archivo, IMC)
+            
+        
+        elif menu=="2":
+            w=Weight()
+            producto=Producto()
+            kC=int(input("Cuantas kiloCalorias tiene tu alimento "))
+            resultadoXGenero=Genero(kC)
+            EnergiaGananda(name, w, producto,kC, resultadoXGenero)
+        
+        elif menu=="3":
+            intensidad=input("¿Prefieres una rutina de ejercicio intensa o relajada? 1=Intensa, 2=Relajada")
+            hacesEjercicio(intensidad)
 
+        elif menu=="4":
+            if len(historialImc) == 0:
+                print("No tienes historial de IMC")
+                print("")
+            else:
+                print(name, "tu IMC de manera cronologica ha sido de (IMC, año)")
+                print(historialImc)
+                print("")
+        elif menu=="5":
+            prueba()
 
+        elif menu=="6":
+            passcode=input("Introduce la contraseña pista:(Es el nombre del creador de este codigo en minusculas):")
+            if(passcode=="ethan"):
+                archivo=open("historial.txt","r")
+                leerFile(archivo)
+            else:
+                print("Not Gut try again")
+                print("")
+        else:
+            break
 
-prueba()
+main()
+#prueba()
